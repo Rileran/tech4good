fc348fd65529822dcda8a9ab70b6cc64f0bd9668
AlexisMelo <Alexis.Robotiik@hotmail.fr>
2022-06-25 13:53:45 +0200

    animations css mutualisé

--- src/animations/Sun.tsx
+++ src/animations/Sun.tsx
@@ -1,19 +1,14 @@
 import Lottie from 'lottie-react';
 import {useCurrentFrame} from 'remotion';
 import animation from '../lottie/sun.json';
+import './animations.scss';
 
 export const Sun = () => {
 	const frame = useCurrentFrame();
 
 	return (
 		<Lottie
-			className="absolute top-0 left-0 h-full"
-			style={{
-				position: 'absolute',
-				top: '0px',
-				left: '0px',
-				height: 'full',
-			}}
+			className="animations"
 			autoplay={false}
 			initialSegment={[frame, frame]}
 			animationData={animation}
