083abd30ae5b8dabc4862eb0925f555b426b2620
Rileran <a.rileran@gmail.com>
2022-06-25 03:25:08 +0200

    feat: scss

--- remotion.config.ts
+++ remotion.config.ts
@@ -6,3 +6,25 @@ import {Config} from 'remotion';
 
 Config.Rendering.setImageFormat('jpeg');
 Config.Output.setOverwriteOutput(true);
+
+Config.Bundling.overrideWebpackConfig((currentConfiguration) => {
+	return {
+		...currentConfiguration,
+		module: {
+			...currentConfiguration.module,
+			rules: [
+				...(currentConfiguration.module?.rules
+					? currentConfiguration.module.rules
+					: []),
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						{loader: 'style-loader'},
+						{loader: 'css-loader'},
+						{loader: 'sass-loader', options: {sourceMap: true}},
+					],
+				},
+			],
+		},
+	};
+});
