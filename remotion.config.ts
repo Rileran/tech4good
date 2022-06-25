76170db5a7f69575309a7e2e42a9467bb94bb4bc
Rileran <a.rileran@gmail.com>
2022-06-25 04:01:32 +0200

    feat: tailwind

--- remotion.config.ts
+++ remotion.config.ts
@@ -28,3 +28,53 @@ Config.Bundling.overrideWebpackConfig((currentConfiguration) => {
 		},
 	};
 });
+
+Config.Bundling.overrideWebpackConfig((currentConfiguration) => {
+	return {
+		...currentConfiguration,
+		module: {
+			...currentConfiguration.module,
+			rules: [
+				...(currentConfiguration.module?.rules
+					? currentConfiguration.module.rules
+					: []
+				).filter((rule) => {
+					if (rule === '...') {
+						return false;
+					}
+					if (rule.test?.toString().includes('.css')) {
+						return false;
+					}
+					return true;
+				}),
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						{loader: 'style-loader'},
+						{loader: 'css-loader'},
+						{loader: 'sass-loader', options: {sourceMap: true}},
+					],
+				},
+				{
+					test: /\.css$/i,
+					use: [
+						'style-loader',
+						'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								postcssOptions: {
+									plugins: [
+										'postcss-preset-env',
+										'tailwindcss',
+										'autoprefixer',
+									],
+								},
+							},
+						},
+					],
+				},
+			],
+		},
+	};
+});
--- src/Wind.tsx
+++ src/Wind.tsx
@@ -2,14 +2,19 @@ import Lottie from 'lottie-react';
 import {useCurrentFrame} from 'remotion';
 import animation from './lottie/wind.json';
 
+import './style.css';
+
 export const Wind = () => {
 	const frame = useCurrentFrame();
 
 	return (
-		<Lottie
-			autoplay={false}
-			initialSegment={[frame, frame]}
-			animationData={animation}
-		/>
+		<div className="relative h-full justify-center align-middle">
+			<Lottie
+				className="absolute top-0 left-0 h-full"
+				autoplay={false}
+				initialSegment={[frame, frame]}
+				animationData={animation}
+			/>
+		</div>
 	);
 };
