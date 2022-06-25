ca10875168a2b16d8f0963587614a3f3a20855cc
AlexisMelo <Alexis.Robotiik@hotmail.fr>
2022-06-25 09:19:11 +0200

    suppression hello world

--- src/Video.tsx
+++ src/Video.tsx
@@ -6,8 +6,6 @@ import {Atmo} from './Atmo';
 import { Aurevoir } from './Aurevoir/Aurevoir';
 import {CityMap} from './CityMap';
 import { Details } from './Details/Details';
-import {HelloWorld} from './HelloWorld';
-import {Logo} from './HelloWorld/Logo';
 import {Score} from './Score/Score';
 import {Title} from './Title/Title';
 
@@ -44,31 +42,30 @@ export const RemotionVideo: React.FC = () => {
 				width={1920}
 				height={1200}
 			/>
-			<Composition
-				// You can take the "id" to render a video:
-				// npx remotion render src/index.tsx <id> out/video.mp4
-				id="HelloWorld"
-				component={HelloWorld}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1200}
-				// You can override these props for each render:
-				// https://www.remotion.dev/docs/parametrized-rendering
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			{/* Mount any React component to make it show up in the sidebar and work on it individually! */}
-			<Composition
-				id="OnlyLogo"
-				component={Logo}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1200}
-			/>
+				<Composition
+					id="Title"
+					component={Title}
+					durationInFrames={150}
+					fps={30}
+					width={1920}
+					height={1200}
+					defaultProps={{
+						date,
+						city: 'Rouen',
+					}}
+				/>
+				<Composition
+					id="Score"
+					component={Score}
+					durationInFrames={150}
+					fps={30}
+					width={1920}
+					height={1200}
+					defaultProps={{
+						date,
+						score: 6,
+					}}
+				/>
 			<Composition
 				id="CityMap"
 				component={CityMap}
@@ -80,18 +77,52 @@ export const RemotionVideo: React.FC = () => {
 					stations: fakeData,
 				}}
 			/>
-			<Composition
-				id="Title"
-				component={Title}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1200}
-				defaultProps={{
-					date,
-					city: 'Rouen',
-				}}
-			/>
+				<Composition
+					id="Details"
+					component={Details}
+					durationInFrames={150}
+					fps={30}
+					width={1920}
+					height={1200}
+					defaultProps={{details: [{
+						title: 'Dioxyde de carbone',
+						cause: '(voitures, agriculture, ...)',
+						quantity: '23 mg/l'
+					},
+					{
+						title: 'Dioxyde de carbone',
+						cause: '(voitures, agriculture, ...)',
+						quantity: '23 mg/l'
+					},
+					{
+						title: 'Dioxyde de carbone',
+						cause: '(voitures, agriculture, ...)',
+						quantity: '23 mg/l'
+					},
+					{
+						title: 'Dioxyde de carbone',
+						cause: '(voitures, agriculture, ...)',
+						quantity: '23 mg/l'
+					},
+					{
+						title: 'Dioxyde de carbone',
+						cause: '(voitures, agriculture, ...)',
+						quantity: '23 mg/l'
+					}
+				],
+				score: 1}}
+				/>
+				<Composition
+					id="Advice"
+					component={Advice}
+					durationInFrames={150}
+					fps={30}
+					width={1920}
+					height={1200}
+					defaultProps={{
+						advice: 'Arrêtez les bagnoles'
+					}}
+				/>
 			<Composition
 				id="Wind"
 				component={Wind}
@@ -99,64 +130,6 @@ export const RemotionVideo: React.FC = () => {
 				fps={30}
 				width={1920}
 				height={1200}
-			/>
-			<Composition
-				id="Score"
-				component={Score}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1200}
-				defaultProps={{
-					date,
-					score: 6,
-				}}
-			/>
-			<Composition
-				id="Advice"
-				component={Advice}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1200}
-				defaultProps={{
-					advice: 'Arrêtez les bagnoles'
-				}}
-			/>
-			<Composition
-				id="Details"
-				component={Details}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1200}
-				defaultProps={{details: [{
-					title: 'Dioxyde de carbone',
-					cause: '(voitures, agriculture, ...)',
-					quantity: '23 mg/l'
-				},
-				{
-					title: 'Dioxyde de carbone',
-					cause: '(voitures, agriculture, ...)',
-					quantity: '23 mg/l'
-				},
-				{
-					title: 'Dioxyde de carbone',
-					cause: '(voitures, agriculture, ...)',
-					quantity: '23 mg/l'
-				},
-				{
-					title: 'Dioxyde de carbone',
-					cause: '(voitures, agriculture, ...)',
-					quantity: '23 mg/l'
-				},
-				{
-					title: 'Dioxyde de carbone',
-					cause: '(voitures, agriculture, ...)',
-					quantity: '23 mg/l'
-				}
-			],
-			score: 1}}
 			/>
 			<Composition
 				id="Aurevoir"
