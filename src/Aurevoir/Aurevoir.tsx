2e53e899da0eac0a12fda7fd2e159440711930ce
AlexisMelo <Alexis.Robotiik@hotmail.fr>
2022-06-25 09:49:23 +0200

    images au bon endroit

--- src/Aurevoir/Aurevoir.tsx
+++ src/Aurevoir/Aurevoir.tsx
@@ -1,10 +1,14 @@
 import './Aurevoir.scss';
+import {Img} from 'remotion';
+import Qualitair from '../assets/Qualitair.svg';
 
 export const Aurevoir = () => {
 	return (
 		<div className="container">
-			<div className="by">Ce bulletin vous a été présenté par</div>
-      <div className="equipe">L&apos;équipe Choux et Carottes</div>
+			<div className="ceBulletin">Ce bulletin</div>
+			<Img src={Qualitair} />
+			<div className="vousA">vous est proposé par l&apos;équipe</div>
+      <div className="equipe">Choux et Carottes</div>
 		</div>
 	);
 };
